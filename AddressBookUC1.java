dc64c993be63df561838748091656943ee159eb3
rajashekar reddy <chrajashekarreddy777@gmail.com>
2021-12-16 17:26:01 +0530

    commiting the changes

--- AddressBookUC1.java
+++ AddressBookUC1.java
@@ -1,5 +1,38 @@
 package com.bl.AddressBook;
 
-public class AddressBookUC1 {
+import java.util.Scanner;
 
+public class AddressBookUC1 {
+	public static void contacts() {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first name");
+		String firstName = sc.next();
+		System.out.println("Enter last name");
+		String lastName = sc.next();
+		System.out.println("Enter housenumber");
+
+		int houseNumber = sc.nextInt();
+		System.out.println("Enter address");
+		String address = sc.next();
+		System.out.println("Enter State");
+		String state = sc.next();
+		System.out.println("Enter zip code");
+		int  zip = sc.nextInt();
+		System.out.println("Enter phone number");
+		long phoneNumber = sc.nextLong();
+		System.out.println("Enter EmailID");
+		String emailID = sc.next();
+		sc.close();
+		System.out.println("==============================================================");
+		System.out.println(" First name : "+firstName+"\n LastName :"+lastName+"\n House Number :"+houseNumber+"\n Address : "+address+"\n State :"+state+"\n zip : "+zip+"\n Phone number :"+phoneNumber+"\n Email ID :"+emailID);
+
+	}
+	public static void main(String[] args) {
+		System.out.println("welcome to AddressBook");
+		contacts();
+	}
 }
+
+
+
